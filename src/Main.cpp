c1d911b93273ca0c114555e094566db911a30ac5
Vishal Dalwadi <dalwadivishal26@gmail.com>
2021-01-13 21:28:10 +0530

    Create SystemContext and Read Configuration

---
+++ src/Main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by mando on 12/01/21.
+//
+
+#include <boost/program_options.hpp>
+#include <iostream>
+#include "SystemContext.h"
+
+namespace po = boost::program_options;
+
+int main(int argc, char** argv) {
+    SystemContext &systemContext = SystemContext::GenerateContext(argc, argv);
+
+    return 0;
+}
---
+++ src/SystemContext.h
@@ -0,0 +1,27 @@
+//
+// Created by mando on 12/01/21.
+//
+
+#ifndef MESSAGE_BROKER_SYSTEM_SYSTEMCONTEXT_H
+#define MESSAGE_BROKER_SYSTEM_SYSTEMCONTEXT_H
+
+#include <string>
+
+
+class SystemContext {
+private:
+    uint16_t port_no;
+    uint16_t num_api_threads;
+    std::string crt_file_path;
+    std::string key_file_path;
+
+    SystemContext() {}
+
+public:
+    SystemContext(SystemContext const&) = delete;
+    void operator=(SystemContext const&) = delete;
+    static SystemContext &GenerateContext(int argc, char **argv);
+};
+
+
+#endif //MESSAGE_BROKER_SYSTEM_SYSTEMCONTEXT_H
