be9b664150fd4bb5085e0e7f0a2d0001b5a0829c
Vishal Dalwadi <dalwadivishal26@gmail.com>
2021-01-29 20:22:29 +0530

    Using Initializer List and privatizing fields.

--- src/Client.cpp
+++ src/Client.cpp
@@ -3,8 +3,3 @@
 //
 
 #include "Client.h"
-
-Client::Client(std::string name, std::string notif_port_no) {
-    this->name = name;
-    this->notif_port_no = notif_port_no;
-}
--- src/Client.h
+++ src/Client.h
@@ -6,13 +6,28 @@
 #define MESSAGE_BROKER_SYSTEM_CLIENT_H
 
 #include <string>
+#include <utility>
 
 class Client {
+private:
+    std::string _name{};
+    std::string _ip_address{};
+    std::string _notif_port_no{};
 public:
-    std::string name{};
-    std::string notif_port_no{};
+    Client(std::string name, std::string ip_address, std::string notif_port_no):
+    _name(std::move(name)), _ip_address(std::move(ip_address)), _notif_port_no(std::move(notif_port_no)) {}
 
-    Client(std::string name, std::string notif_port_no);
+    std::string& name() {
+        return _name;
+    }
+
+    std::string& ip_address() {
+        return _ip_address;
+    }
+
+    std::string& notif_port_no() {
+        return _notif_port_no;
+    }
 };
 
 
