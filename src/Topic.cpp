6eddab7f7a3317954e08db33dac662c5711733e3
Vishal Dalwadi <dalwadivishal26@gmail.com>
2021-02-27 01:29:52 +0530

    Modified Topic class to store events as vectors

--- src/Topic.cpp
+++ src/Topic.cpp
@@ -4,19 +4,44 @@
 
 #include "Topic.h"
 
+#include <utility>
+
 Topic::Topic(string topic_name) {
     this->name = std::move(topic_name);
+    this->num_active_clients = 0;
 }
 
 Topic& Topic::operator=(const Topic& topic) {
     return *this;
 }
 
-void Topic::pub_event(Event event) {
+void Topic::pub_event(std::string event) {
     mutex_lock.lock();
-    this->events.push_back(std::move(event));
+    count_mutex_lock.lock();
+    events.emplace_back(std::move(event), num_active_clients);
+    num_events_published++;
+    count_mutex_lock.unlock();
     mutex_lock.unlock();
 }
-Event Topic::get_event() const {
-    return events.front();
+Event Topic::get_event_for(Client& client) {
+    long index = num_events_published - client.num_events_fetched_from(name) - 1;
+    Event& event = events[index];
+    event.decrement_count();
+    if (event.read_by_all_clients()) {
+        events.erase(events.begin());
+    }
+    client.increment_num_events_fetched_from(name);
+    return event;
+}
+
+void Topic::increment_num_active_clients() {
+    count_mutex_lock.lock();
+    num_active_clients++;
+    count_mutex_lock.unlock();
+}
+
+void Topic::decrement_num_active_clients() {
+    count_mutex_lock.lock();
+    num_active_clients--;
+    count_mutex_lock.unlock();
 }
--- src/Topic.h
+++ src/Topic.h
@@ -8,19 +8,25 @@
 #include <utility>
 #include <mutex>
 #include "Event.h"
+#include "Client.h"
 
 
-class Topic {
+class Topic : public basic_string<char> {
 private:
     std::mutex mutex_lock;
+    std::mutex count_mutex_lock;
+    long num_events_published;
+    long num_active_clients{};
 public:
     std::string name;
-    list<Event> events{};
+    vector<Event> events{};
     Topic() = default;
     explicit Topic(string topic_name);
     Topic& operator=(const Topic& topic);
-    void pub_event(Event event);
-    Event get_event() const;
+    void pub_event(std::string event);
+    Event get_event_for(Client& client);
+    void increment_num_active_clients();
+    void decrement_num_active_clients();
 };
 
 #endif
